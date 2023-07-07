591d7b4b9b7327ddcc75c3be7465c31d3638b729
Vincent Novianto <vincentnovianto16@gmail.com>
2023-07-07 08:14:34 +0700

    Add samting

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Sudah tak ubah yak
         </p>
         <a
           className="App-link"
