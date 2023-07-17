ea48b2497ebc508db4dbd0437ccf29f707a0b24b
Vincent Novianto <vincentnovianto16@gmail.com>
2023-07-17 12:07:11 +0700

    again2

--- src/Materi/Komponen/index.jsx
+++ src/Materi/Komponen/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ClassComponent from "./pembahasan/ClassComponent";
 import FunctionalComponent from "./pembahasan/FunctionalComponent";
 import TestComponent from "./pembahasan/TestComponent";
-import Porto from "./Porto";
+// import Porto from "./Porto";
 
 
 export default class Komponen extends React.Component{
